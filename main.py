0ea4642bc550bb669f6511cdd56cfef9d7fc376d
Sergey <dddf4014@gmail.com>
2024-07-09 23:43:38 +1000

    console version

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-# import keyboard
-# import mouse
-# import time
+import keyboard
+import mouse
+import time
 
-# isClicking = False
+isClicking = False
 
-# def set_clikcer():
-#     global isClicking
-#     if isClicking:
-#         isClicking = False
-#         print("Кликер отключен")
-#     else:
-#         isClicking = True
-#         print("Кликер включен")
+def set_clikcer():
+    global isClicking
+    if isClicking:
+        isClicking = False
+        print("Кликер отключен")
+    else:
+        isClicking = True
+        print("Кликер включен")
     
-# keyboard.add_hotkey("Alt + Z", set_clikcer)
+keyboard.add_hotkey("Alt + Z", set_clikcer)
 
-# while True:
-#     if self.isClicking:
-#         mouse.double_click(button = "left")
-#         time.sleep(0.01)
-
-from GUI import GUI
-
-if __name__ == "__main__":
-    GUI()
+while True:
+    if isClicking:
+        mouse.double_click(button = "left")
+        time.sleep(0.01)
 
